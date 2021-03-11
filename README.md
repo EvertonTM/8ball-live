41c3c1c6c5e7560f60c569b676b1e87e8abcbb71
EvertonTM <EvertonTM@users.noreply.github.com>
2021-03-10 23:34:32 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,85 @@
+- **Importante**
+
+*Não me responsabilizo pelo uso ilícito deste hack*
+
+É preciso que você faça algumas alterações para ajustar o layout à mesa do jogo
+
+> Anti-ban, apenas se você seguir estes passos:
+
+- Não utilize o mesmo *nome de pacote* ( app.hack.eightballpool )
+
+- Crie um *nome de pacote* único para o seu celular e não compartilhe usando o mesmo nome
+
+- Não publique na Play Store
+
+É recomendável que você também altere o nome do aplicativo
+
+*Atenção:* Não teste o hack em seu celular sem seguir os passos acima
+
+> Após você fechar e quiser exibir o Menu novamente, balance o celular
+
+- **Important**
+
+*I am not responsible for the illegal use of this hack*
+
+You need to make some changes to adjust the layout to the game table
+
+> Anti-ban, only if you follow these steps:
+
+- Do not use the same *package name* ( app.hack.eightballpool )
+
+- Create a unique *package name* for your smartphone and don't share using the same name
+
+- Do not post on the Play Store
+
+It is recommended that you also change the name of the app
+
+*Attention:* Do not test the hack on your smartphone without following the above steps
+
+> After you close and want to display the Menu again, shake your smartphone
+
+### Links
+
+https://github.com/jonathansilva/8BallPool/blob/master/app/src/main/res/values/dimens.xml
+
+https://github.com/jonathansilva/8BallPool/blob/master/app/src/main/res/values/strings.xml#L3
+
+https://github.com/jonathansilva/8BallPool/blob/master/settings.gradle#L2
+
+### Videos
+
+> Normal
+
+https://www.youtube.com/watch?v=pH-uqXHBreI
+
+> Trickshot
+
+https://www.youtube.com/watch?v=mnTPQZCr1y0
+
+> 9 Ball
+
+https://www.youtube.com/watch?v=1m9RLHiNUOI
+
+### Screenshots
+
+> Normal
+
+![Screenshot_20201112-181204](https://user-images.githubusercontent.com/33843748/98998412-dd75f180-2514-11eb-8a3b-1a93d822d01a.png)
+
+![Screenshot_20201112-175715](https://user-images.githubusercontent.com/33843748/98998449-e666c300-2514-11eb-9b5f-dcf300065650.png)
+
+> Trickshot
+
+![Screenshot_20201112-175443](https://user-images.githubusercontent.com/33843748/98998464-ee266780-2514-11eb-8880-e9b16854459b.png)
+
+![Screenshot_20201112-180023](https://user-images.githubusercontent.com/33843748/98998488-f7afcf80-2514-11eb-86fd-c8105c3d0494.png)
+
+> 9 Ball
+##### Apenas um guia. A vitória em uma tacada não funciona 100%
+##### Just a guide. One shot win does not work 100%
+
+![screenshot-nineball-one](https://user-images.githubusercontent.com/33843748/100782264-ee66a400-33ea-11eb-97a9-b986373829f2.jpg)
+
+![screenshot-nineball-two](https://user-images.githubusercontent.com/33843748/100782268-ef97d100-33ea-11eb-9b7b-8ca355c8d2a5.jpg)
+
+![screenshot-nineball-three](https://user-images.githubusercontent.com/33843748/100782266-eeff3a80-33ea-11eb-8e54-6382d3579fa3.jpg)
---
+++ app/src/main/java/app/hack/eightballpool/Normal.java
@@ -0,0 +1,170 @@
+package app.hack.eightballpool;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class Normal extends View {
+    private Paint lineTopLeft;
+    private Paint lineTopRight;
+    private Paint lineBottomLeft;
+    private Paint lineBottomRight;
+    private Paint lineMiddleTop;
+    private Paint lineMiddleBottom;
+    private Paint circle;
+
+    float xOnTouch, yOnTouch;
+    float xOnMotion, yOnMotion;
+    float xCircle, yCircle;
+
+    int radius = 100;
+    int strokeWidth = 6;
+    int shadowRadius = 6;
+
+    boolean trackStatus;
+
+    public Normal(Context context) {
+        super(context);
+
+        init();
+    }
+
+    public Normal(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        init();
+    }
+
+    public Normal(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        init();
+    }
+
+    private void init() {
+        lineMiddleTop = new Paint();
+        lineTopLeft = new Paint();
+        lineTopRight = new Paint();
+        lineMiddleBottom = new Paint();
+        lineBottomLeft = new Paint();
+        lineBottomRight = new Paint();
+        circle = new Paint();
+
+        lineMiddleTop.setStrokeWidth(strokeWidth);
+        lineMiddleTop.setStrokeCap(Paint.Cap.ROUND);
+        lineMiddleTop.setColor(Color.WHITE);
+        lineMiddleTop.setShadowLayer(shadowRadius, 0, 0, Color.BLACK);
+        lineMiddleTop.setAntiAlias(true);
+
+        lineTopLeft.setStrokeWidth(strokeWidth);
+        lineTopLeft.setStrokeCap(Paint.Cap.ROUND);
+        lineTopLeft.setColor(Color.WHITE);
+        lineTopLeft.setShadowLayer(shadowRadius, 0, 0, Color.BLACK);
+        lineTopLeft.setAntiAlias(true);
+
+        lineTopRight.setStrokeWidth(strokeWidth);
+        lineTopRight.setStrokeCap(Paint.Cap.ROUND);
+        lineTopRight.setColor(Color.WHITE);
+        lineTopRight.setShadowLayer(shadowRadius, 0, 0, Color.BLACK);
+        lineTopRight.setAntiAlias(true);
+
+        lineMiddleBottom.setStrokeWidth(strokeWidth);
+        lineMiddleBottom.setStrokeCap(Paint.Cap.ROUND);
+        lineMiddleBottom.setColor(Color.WHITE);
+        lineMiddleBottom.setShadowLayer(shadowRadius, 0, 0, Color.BLACK);
+        lineMiddleBottom.setAntiAlias(true);
+
+        lineBottomLeft.setStrokeWidth(strokeWidth);
+        lineBottomLeft.setStrokeCap(Paint.Cap.ROUND);
+        lineBottomLeft.setColor(Color.WHITE);
+        lineBottomLeft.setShadowLayer(shadowRadius, 0, 0, Color.BLACK);
+        lineBottomLeft.setAntiAlias(true);
+
+        lineBottomRight.setStrokeWidth(strokeWidth);
+        lineBottomRight.setStrokeCap(Paint.Cap.ROUND);
+        lineBottomRight.setColor(Color.WHITE);
+        lineBottomRight.setShadowLayer(shadowRadius, 0, 0, Color.BLACK);
+        lineBottomRight.setAntiAlias(true);
+
+        circle.setStyle(Paint.Style.STROKE);
+        circle.setStrokeWidth(10);
+        circle.setColor(getContext().getColor(R.color.colorRed));
+        circle.setAntiAlias(true);
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                xOnTouch = event.getX();
+                yOnTouch = event.getY();
+
+                trackStatus = xOnTouch > (xCircle - radius)
+                        && xOnTouch < (xCircle + radius)
+                        && yOnTouch > (yCircle - radius)
+                        && yOnTouch < (yCircle + radius);
+
+                break;
+            case MotionEvent.ACTION_MOVE:
+                xOnMotion = event.getX();
+                yOnMotion = event.getY();
+
+                xCircle = xOnMotion;
+                yCircle = yOnMotion;
+
+                // Top
+                if (yCircle < 0) {
+                    yCircle = 0;
+                }
+
+                // Bottom
+                if (yCircle > getHeight()) {
+                    yCircle = getHeight();
+                }
+
+                // Left
+                if (xCircle < 0) {
+                    xCircle = 0;
+                }
+
+                // Right
+                if (xCircle > getWidth()) {
+                    xCircle = getWidth();
+                }
+
+                trackStatus = true;
+                invalidate();
+                break;
+            case MotionEvent.ACTION_UP:
+                trackStatus = false;
+                break;
+        }
+
+        return trackStatus;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        canvas.drawLine(xCircle, yCircle - radius, getWidth() / 2f, 3, lineMiddleTop);
+        canvas.drawLine(xCircle, yCircle + radius, getWidth() / 2f, getHeight() - 3, lineMiddleBottom);
+        canvas.drawLine(xCircle - (70 / 100f * radius), yCircle - (70 / 100f * radius), 10, 10, lineTopLeft);
+        canvas.drawLine(xCircle + (70 / 100f * radius), yCircle - (70 / 100f * radius), getWidth() - 10, 10, lineTopRight);
+        canvas.drawLine(xCircle - (70 / 100f * radius), yCircle + (70 / 100f * radius), 10, getHeight() - 10, lineBottomLeft);
+        canvas.drawLine(xCircle + (70 / 100f * radius), yCircle + (70 / 100f * radius), getWidth() - 10, getHeight() - 10, lineBottomRight);
+
+        canvas.drawCircle(xCircle, yCircle, radius, circle);
+    }
+
+    public void setPositionCircle(float x, float y) {
+        xCircle = x;
+        yCircle = y;
+    }
+}
